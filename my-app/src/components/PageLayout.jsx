40bd01349a830873baf6a59f2e88afe50ceec342
P Vaibhav <pvaibhavofficial@gmail.com>
2024-06-30 17:57:32 +0530

    solved border margin in xl screen issue

--- my-app/src/components/PageLayout.jsx
+++ my-app/src/components/PageLayout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PageLayout = ({ children }) => {
   return (
-    <div className="container mx-auto px-4 sm:px-6 lg:px-0 py-4 overflow-hidden">
+    <div className="container mx-auto px-4 sm:px-6 xl:px-24 2xl:px-4 py-4 overflow-hidden">
       {children}
     </div>
   );
